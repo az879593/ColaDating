ea3ba32594033ef7966d28104239f32f9de695c7
Ruby Lee <as97108@gmail.com>
2022-12-19 01:36:44 +0800

    modify session_helper

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -7,3 +7,11 @@
     function flash($name = '', $message = '', $class = 'alert alert-success'){
         
     }
+
+    function isLoggedIn(){
+        if(isset($_SESSION['user_id'])){
+            return true;
+        } else {
+            return false;
+        }
+    }
